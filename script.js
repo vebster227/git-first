9d345b397f06fbc74a4314a0083eee12e6041e33
vebster227 <vebster2580@gmail.com>
2023-07-21 09:32:41 +0200

    practise with js/html

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-const a = 1;
-const b = -9;
-console.log(b + a);
+class Chart {
+    constructor(height, background, hasBorder) {
+        this.height = height;
+        this.background = background;
+        this.hasBorder = hasBorder;
+    }
+}
+
+const chartContent = new Chart("300px", "red", true);
+const chartContent2 = new Chart("200px", "green", true);
+const chartContent3 = new Chart("259px", "blue", false);
+
+
+
+
+function renderChart(charData) {
+    const chartsContainer = document.querySelector(".charts-container");
+    const chart = document.createElement("div");
+    chart.classList.add("chart");
+    chart.style.height = charData.height;
+    chart.style.backgroundColor = charData.background;
+    if (charData.hasBorder) {
+        chart.style.border = "2px solid black";
+    }
+    chartsContainer.appendChild(chart);
+}
+
+renderChart(chartContent);
+renderChart(chartContent2);
+renderChart(chartContent3);
